2a25f015340f6957d754f7c35fc7bbecca7192c9
thuyihan1206 <chchan1206@gmail.com>
2020-08-16 15:27:57 -0400

    added a second partition method

--- quicksort.rb
+++ quicksort.rb
@@ -10,20 +10,37 @@ def sort(arr, left = 0, right = arr.length - 1)
   arr
 end
 
-def partition(arr, left, right)
-  pivot = arr[left]
-  up = left
-  down = right
-  loop do
-    # pivot is greater than or equal to its left elements and less than its right elements
-    up += 1 while up < right && pivot >= arr[up]
-    down -= 1 while pivot < arr[down]
-    break unless up < down
+# one way
+# def partition(arr, left, right)
+#   pivot = arr[left]
+#   up = left
+#   down = right
+#   loop do
+#     # pivot is greater than or equal to its left elements and less than its right elements
+#     up += 1 while up < right && pivot >= arr[up]
+#     down -= 1 while pivot < arr[down]
+#     break unless up < down
 
-    arr[down], arr[up] = arr[up], arr[down] # exchange up and down
+#     arr[down], arr[up] = arr[up], arr[down] # exchange up and down
+#   end
+#   arr[down], arr[left] = arr[left], arr[down] # exchange left and down
+#   down
+# end
+
+# second way: this requires two auxiliary arrays and more changes, but easier to understand
+def partition(arr, left, right)
+  arr_left = []
+  arr_right = []
+  pivot = arr[left]
+  arr[left + 1..right].each do |element|
+    if element <= pivot
+      arr_left << element
+    else
+      arr_right << element
+    end
   end
-  arr[down], arr[left] = arr[left], arr[down] # exchange left and down
-  down
+  arr[left..right] = arr_left + [pivot] + arr_right
+  left + arr_left.length
 end
 
 arr = [1, 3, 98, 2, 3, 6, 4, 0, 1]
