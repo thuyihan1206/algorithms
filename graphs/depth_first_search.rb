353e68d25286bd667b8dee22d868f827251f05fb
thuyihan1206 <chchan1206@gmail.com>
2020-08-16 22:18:00 -0400

    updated comment

--- graphs/depth_first_search.rb
+++ graphs/depth_first_search.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # depth first search
-# note: we check if a vertex has been visited after popping
+# note: we check if a vertex has been discovered after popping
 def dfs(adj_matrix, source_vertex, target_vertex)
   stack = [source_vertex]
   visited = [] # used to keep track of visited order
