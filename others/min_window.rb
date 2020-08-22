afdd15ab2ad2b29e74cba8fd8a847d4b2edc923b
thuyihan1206 <chchan1206@gmail.com>
2020-08-22 15:25:50 -0400

    min window classic problem

---
+++ others/min_window.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# given a string S and a string T, find the minimum window in S which will contain all the characters in T
+def min_window(s, t)
+  return nil if s.length < t.length
+
+  # create hash for string T
+  t_hash = {}
+  t.each_char do |c|
+    t_hash[c] ||= 0
+    t_hash[c] += 1
+  end
+
+  left_start = nil
+  count = 0
+  min_window = nil
+
+  s.each_char.with_index do |c, right_index|
+    next unless t_hash[c]
+
+    left_start ||= right_index
+    count += 1 if t_hash[c] == 1
+    t_hash[c] -= 1
+
+    next unless count == t_hash.length
+
+    min_window = s[left_start..right_index] # found min_window the first time
+
+    while count == t_hash.length
+      if t_hash[s[left_start]]
+        t_hash[s[left_start]] += 1
+        count -= 1 if t_hash[s[left_start]] == 1
+      end
+      left_start += 1
+    end
+    left_index = left_start - 1 # back one to include the valid character
+    min_window = s[left_index..right_index] if right_index - left_index + 1 < min_window.length
+  end
+  min_window
+end
+
+p min_window('dcbefebce', 'fd') # => "dcbef"
+p min_window('dcdefebce', 'fd') # => "def"
+p min_window('dcbefebdf', 'fd') # => "df"
+p min_window('ecbefebce', 'fd') # => nil
+p min_window('dcbefedce', 'fdd') # => "dcbefed"
+p min_window('dcbefebce', 'bcdecebfe') # => "dcbefebce"
+p min_window('dcbefedce', 'fddfddfddfdd') # => nil
