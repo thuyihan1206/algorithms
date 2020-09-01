6de34786e6050975afbbcfe119f14fc5de2aeea3
thuyihan1206 <chchan1206@gmail.com>
2020-09-01 18:01:22 -0400

    uses first way for partition since its more efficient

--- sorting/quicksort.rb
+++ sorting/quicksort.rb
@@ -10,25 +10,25 @@ def sort(arr, left = 0, right = arr.length - 1)
   arr
 end
 
-# one way
-# def partition(arr, left, right)
-#   pivot = arr[left]
-#   up = left
-#   down = right
-#   loop do
-#     # pivot is greater than or equal to its left elements and less than its right elements
-#     up += 1 while up < right && pivot >= arr[up]
-#     down -= 1 while pivot < arr[down]
-#     break unless up < down
+# first way
+def partition(arr, left, right)
+  pivot = arr[left]
+  up = left
+  down = right
+  loop do
+    # pivot is greater than or equal to its left elements and less than its right elements
+    up += 1 while up < right && pivot >= arr[up]
+    down -= 1 while pivot < arr[down]
+    break if up >= down
 
-#     arr[down], arr[up] = arr[up], arr[down] # exchange up and down
-#   end
-#   arr[down], arr[left] = arr[left], arr[down] # exchange left and down
-#   down
-# end
+    arr[down], arr[up] = arr[up], arr[down] # exchange up and down
+  end
+  arr[down], arr[left] = arr[left], arr[down] # exchange left and down
+  down
+end
 
 # second way: this requires two auxiliary arrays and more changes, but easier to understand
-def partition(arr, left, right)
+def partition2(arr, left, right)
   arr_left = []
   arr_right = []
   pivot = arr[left]
